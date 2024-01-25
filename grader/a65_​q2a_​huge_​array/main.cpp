981daf4813bc0c5750efbc4490fd24ccafb8ce5f
tanasinp <wavet888@gmail.com>
2024-01-25 09:37:29 +0700

    .

---
+++ grader/a65_​q2a_​huge_​array/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n,q;
+    cin >> n >> q;
+    vector<pair<int,int>> v;
+    
+    for(int i=0;i<n;i++){
+
+    }
+}
