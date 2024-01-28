b864d62b6a34e4b29ac9b8a96c58d59dcced155b
tanasinp <wavet888@gmail.com>
2024-01-28 13:41:54 +0700

    dt

--- grader/a60b_​mid_​p4_​merge/main.cpp
+++ grader/a60b_​mid_​p4_​merge/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 int main(){
-    
+    vector<int> v;
+    int n,k;
+    cin >> n >> k;
+    if (k%2 == 0) cout << "-1" << "\n";
+    for (int i=1 ; i<=n ; i++){
+        v.push_back(i);
+    }
+    k -= 1;
+    while (k){
+        
+        k-= 2;
+    }
+    for (auto &x : v) {
+        cout << x << " ";
+    }
+    cout << "\n";
 }
