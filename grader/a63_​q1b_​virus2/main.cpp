dab576823e84faeb9350946652f4ed37635b994f
tanasinp <wavet888@gmail.com>
2024-02-05 10:45:33 +0700

    .

---
+++ grader/a63_​q1b_​virus2/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int isVirus(vector<int> &v,int r,int l){
+    
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int len = 1<<m;
+}
--- grader/ex02h1_​closest/main.cpp
+++ grader/ex02h1_​closest/main.cpp
@@ -6,4 +6,13 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+    vector<vector<int>> v = v(n);
+    for(int i=0;i<n;i++){
+        while(2--){
+            int p;
+            cin >> p;
+            v[i].push_back(p);
+        }
+    }
+    
 }
