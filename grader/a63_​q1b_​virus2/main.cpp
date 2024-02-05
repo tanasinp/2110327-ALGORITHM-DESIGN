dcf3f92f4099b8fc6d9d7d959ba67c58174c72d1
tanasinp <wavet888@gmail.com>
2024-02-05 13:25:58 +0700

    100

--- grader/a63_​q1b_​virus2/main.cpp
+++ grader/a63_​q1b_​virus2/main.cpp
@@ -4,26 +4,20 @@
 using namespace std;
 
 bool isVirus(string &v,int l,int r){
-    if(r-l == 3){
-        int one_left = 0;
-        int one_right = 0;
-        for (auto i=l ; i <= l+1 ; i++){
-            if (v[i] != '0' && v[i] != '1') return false;
-            if (v[i] == '1') one_left += 1;
-        }
-        for (auto i=r ; i >= r-1 ; i--){
-            if (v[i] != '0' && v[i] != '1') return false;
-            if (v[i] == '1') one_right += 1;
-        }
-        cout << "l : " << l << " r : " << r << "\n";
-        cout << "oneleft : " << one_left << " " << "oneright : " << one_right << "\n";
-        if (one_right-one_left <= 1 && one_right-one_left >= -1) return true;
-        return false;
-    } 
-    int mid = (r+l)/2;
-    bool check_left = isVirus(v,l,mid);
-    bool check_right = isVirus(v,mid+1,r);
-    return (check_left&&check_right);
+    if(r-l == 1) return true;
+    int one_left = 0;
+    int one_right = 0;
+    int mid = (l+r)/2;
+    for(int i=l;i<=mid;i++){
+        if(v[i] == '1') one_left += 1;
+    }
+    for(int i=mid+1;i<=r;i++){
+        if(v[i] == '1') one_right += 1;
+    }
+    cout << "l : " << l << " r : " << r << "\n";
+    cout << "oneleft : " << one_left << " " << "oneright : " << one_right << "\n";
+    if (abs(one_left - one_right) > 1) return false;
+    return (isVirus(v,l,mid) && isVirus(v,mid+1,r));
 }
 
 int main(){
