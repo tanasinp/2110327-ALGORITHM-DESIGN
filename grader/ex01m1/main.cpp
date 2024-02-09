2181896a54ab6b97b8a9f6c3382cf3a126ddfbfa
tanasinp <wavet888@gmail.com>
2024-02-09 13:51:03 +0700

    .

--- grader/ex01m1/main.cpp
+++ grader/ex01m1/main.cpp
@@ -1,62 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
-
-int x,y;
-
-void tile(int x,int y,int l,int type){
-    if (l == 2){
-        cout << type << " " << x << " " << y << "\n";
-    } else {
-        if(type == 0){
-            tile(x+l/2,y+l/2,l/2,0);
-            tile(x+l/2-l/4,y+l/2-l/4,l/2 ,0);
-            tile(x,y+l/2,l/2,1);
-            tile(x+l/2,y,l/2,2);
-        } else if (type == 1){
-            tile();
-            tile();
-            tile();
-            tile();
-        } else if (type == 2){
-            tile();
-            tile();
-            tile();
-            tile();
-        } else if (typr == 3){
-            tile();
-            tile();
-            tile();
-            tile();
+int L,X,Y;
+void build(int px,int py,int l,int type){
+    if(l==2){
+        cout<<type<<" "<<px<<" "<<py<<"\n";
+        return;
+    }else{
+        if(type==0){
+            px+=l/2;
+            py+=l/2;
+            build(px,py,l/2,0);
+            build(px-l/4,py-l/4,l/2,0);
+            build(px-l/2,py,l/2,1);
+            build(px,py-l/2,l/2,2);
         }
+        else if(type==1){
+            py+=l/2;
+            build(px,py,l/2,1);
+            build(px+l/4,py-l/4,l/2,1);
+            build(px+l/2,py,l/2,0);
+            build(px,py-l/2,l/2,3);
+        }
+        else if(type==2){
+            px+=l/2;
+            build(px,py,l/2,2);
+            build(px-l/4,py+l/4,l/2,2);
+            build(px-l/2,py,l/2,3);
+            build(px,py+l/2,l/2,0);
+        }
+        else if(type==3){
+            build(px,py,l/2,3);
+            build(px+l/4,py+l/4,l/2,3);
+            build(px+l/2,py,l/2,2);
+            build(px,py+l/2,l/2,1);
+        }
+        return;
     }
 }
-
-void build_tile(int x,int y,int l){
-    if (l < 2) return;
-    if (x<(l/2) && y<(l/2)){
-        tile(x,y,l,0);
-        bulid_tile(x,y,l/2);
+void buildTile(int x,int y,int l){
+    if(l<2)return;
+    if(X<x+l/2&&Y<y+l/2){
+        build(x,y,l,0);
+        buildTile(x,y,l/2);
+    }else if(X>=x+l/2&&Y<y+l/2){
+        build(x,y,l,1);
+        buildTile(x+l/2,y,l/2);
+    }else if(X<x+l/2&&Y>=y+l/2){
+        build(x,y,l,2);
+        buildTile(x,y+l/2,l/2);
+    }else{
+        build(x,y,l,3);
+        buildTile(x+l/2,y+l/2,l/2);
     }
-    else if (x >= (l/2) && y < (l/2)){
-        tile(x,y,l,1);
-        bulid_tile(x+(l/2),y,l/2);
-    }
-    else if (x < (l/2) && y >= (l/2)){
-        tile(x,y,l,2);
-        bulid_tile(x,y+(l/2),l/2);
-    }
-    else if (x >= (l/2) && y >= (l/2)){
-        tile(x,y,l,3);
-        bulid_tile(x+(l/2),y+(l/2),l/2);
-    }
+    return;
 }
 
-int main(){
-    int n;
-    cin >> n >> x >> y;
-    cout << (n*n-1)/3 << "\n";
-    bulid_tile(x,y,n);
+int main()
+{
+    cin>>L>>X>>Y;
+    cout<<(L*L-1)/3<<"\n";
+    buildTile(0,0,L);
     return 0;
 }
