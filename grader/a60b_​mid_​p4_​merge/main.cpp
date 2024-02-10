d93a20643f936463546aa2ecbfb4c9c62adedc10
tanasinp <wavet888@gmail.com>
2024-02-10 17:46:17 +0700

    .

--- grader/a60b_​mid_​p4_​merge/main.cpp
+++ grader/a60b_​mid_​p4_​merge/main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+int k;
+
+void meow(vector<int> &v,int l,int r){
+    if(k == 1) return;
+    if (r-l == 1) return;
+    k -= 2;
+    int mid = (l+r)/2;
+    // cout << "k : " << k << " mid,mid+1 : " << mid << " " << mid+1 << "\n";
+    swap(v[mid-1],v[mid]);
+    meow(v,l,mid);
+    meow(v,mid,r);
+}
+
 int main(){
-    vector<int> v;
-    int n,k;
+    int n;
     cin >> n >> k;
-    if (k%2 == 0) cout << "-1" << "\n";
-    for (int i=1 ; i<=n ; i++){
+    vector<int> v;
+    for (int i=1;i<=n;i++){
         v.push_back(i);
     }
-    k -= 1;
-    while (k){
-        
-        k-= 2;
+    if (k%2 == 0){
+        cout << "-1\n";
+    } else {
+        meow(v,0,n);
+        if (k != 1) {
+            cout << "-1\n";
+        } else {
+            for (auto &x : v){
+                cout << x << " ";
+            }
+            cout << "\n";
+        }
     }
-    for (auto &x : v) {
-        cout << x << " ";
-    }
-    cout << "\n";
 }
