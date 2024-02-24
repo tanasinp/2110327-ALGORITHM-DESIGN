ea3494c0f13c5821786eb987d1f19699e2302338
tanasinp <wavet888@gmail.com>
2024-02-24 22:34:01 +0700

    bottom up

--- grader/a62_​q1a_​jumping/main.cpp
+++ grader/a62_​q1a_​jumping/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int n;
 vector<int> v;
 vector<int> t;
-int td(int n){
+int td(int n){// using top douwn
     if(n == 0) return v[0];
     if(n < 0) return -9999;
     if(t[n] != -1) return t[n];
@@ -22,5 +22,17 @@ int main(){
     for(int i=0;i<n;i++){
         cin >> v[i];
     }
-    cout << td(n-1) << "\n";
+    // cout << td(n-1) << "\n";
+
+    //using bottom up
+    vector<int> bt(n);
+    for(int i=0;i<n;i++){
+        if(i == 0) bt[i] = v[i];
+        else if(i == 1) bt[i] = v[i] + bt[i-1];
+        else if(i == 2) bt[i] = max(bt[i-2],bt[i-1]) + v[i];
+        else {
+            bt[i] = max(bt[i-3],max(bt[i-2],bt[i-1])) + v[i];
+        }
+    }
+    cout << bt[n-1] << "\n";
 }
