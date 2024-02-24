8495d76e451cb9aeac3043e215b9c29cd49da778
tanasinp <wavet888@gmail.com>
2024-02-24 14:23:41 +0700

    dp.ba

--- grader/ex03m1_​billboard/main.cpp
+++ grader/ex03m1_​billboard/main.cpp
@@ -10,7 +10,17 @@ int main(){
     cin >> n;
     v.resize(n);
     for(int i=0;i<n;i++) cin >> v[i];
-    vector<vector<int>> v[2];
-    for (aut0 x : v) x.resize(n);
     
+    vector<vector<int>> dp(2,vector<int>(n));
+    
+    dp[0][0] = v[0]; 
+    dp[1][0] = 0;
+    dp[0][1] = v[1];
+    dp[1][1] = max(dp[0][0],dp[1][0]);
+    for(int i=2;i<n;i++){
+        dp[0][i] = max(dp[0][i-2],dp[1][i-2]) + v[i];
+        dp[1][i] = max(dp[0][i-1],dp[1][i-1]);
+    }
+    int ans = max(dp[0][n-1],dp[1][n-1]);
+    cout << ans << "\n";
 }
