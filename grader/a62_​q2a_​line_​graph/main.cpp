91e2cd8db07b8e06bee736e02a67187550d7df90
tanasinp <wavet888@gmail.com>
2024-03-24 02:36:53 +0700

    .

--- grader/a62_​q2a_​line_​graph/main.cpp
+++ grader/a62_​q2a_​line_​graph/main.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 int v,e;
 vector<int> al[10101];
-vector<int> visited;
+vector<bool> visited;
+
+int dfs(int current, int last){
+    if(visited[current]) return 0;
+    if(al[current].size() > 2) return 0;
+    int ans = 1;
+    visited[current] = true;
+    for(int i=0;i<al[current].size();i++){
+        if(al[current][i] != last){
+            if(ans!=0) ans = dfs(al[current][i], current);
+        }
+    }
+    return ans;
+}
+
 int main(){
     cin >> v >> e;
     for(int i=0;i<e;i++){
@@ -14,4 +28,10 @@ int main(){
     }
     visited.resize(v);
     
+    int ans = 0;
+    for(int node=0;node<v;node++){
+        if(visited[node]) continue;
+        else ans += dfs(node,node);
+    }
+    cout << ans << "\n";
 }
