7c9199e911a2edfe523967d64e66d83ffdc692c1
tanasinp <wavet888@gmail.com>
2024-03-27 23:26:21 +0700

    10

--- grader/a58_​q3_​p3_​PandG/main.cpp
+++ grader/a58_​q3_​p3_​PandG/main.cpp
@@ -28,7 +28,12 @@ int main(){
         queue<pair<pair<int,int>,pair<pair<int,int>,int>>> q;
         for(int i=0;i<n;i++){
             cin >> ti >> ri >> ci;
-            q.push({{ri,ci},{{ti,1},1}}); // {r,c},{ti,T}
+            if(ti == 0){
+                q.push({{ri,ci},{{ti,1},1}}); // {r,c},{ti,T}
+            } else {
+                q.push({{ri,ci},{{ti,0},1}}); // {r,c},{ti,T}
+            }
+            // q.push({{ri,ci},{{ti,1},1}}); // {r,c},{ti,T}
         }
         q.push({{r,c},{{-1,1},0}});
         for(int i=0;i<R;i++){
@@ -48,31 +53,44 @@ int main(){
                 int nr = cr+mx[i];
                 int nc = cc+my[i];
                 if(0 <= nr && nr < R && 0 <= nc && nc < C && cd <= T){
-                    if(cti >= 0){
-                        if(type == 1 && cti == 0){
-                            g[cr][cc] = 2;
-                            q.push({{nr,nc},{{cti-1,cd+1},type}});
-                        } else {
-                            q.push({{nr,nc},{{cti-1,cd+1},type}});
+                    if(type == 1){//ghost
+                        if(cti == 0){
+                            if(g[cr][cc] < 4) g[cr][cc] = 2;
+                            if(g[nr][nc] < 2){
+                                q.push({{nr,nc},{{cti-1,cd+1},type}});
+                            }
+                        } else if(cti < 0){
+                            if(g[nr][nc] < 2) {
+                                g[nr][nc] = 2;
+                                q.push({{nr,nc},{{cti-1,cd+1},type}});
+                            }
+                        } else {// > 0
+                            q.push({{cr,cc},{{cti-1,cd+1},type}});
                         }
-                    } else {
-                        if(type == 1){ //ghost
-                            if(g[nr][nc] < 2) g[nr][nc] = 2;
-                            q.push({{nr,nc},{{cti-1,cd+1},1}});
-                        } else if(type == 0 && g[cr][cc] == 1){ //pacman
+                    } else {//PacMan
+                        if(g[cr][cc] == 1){
                             if(g[nr][nc] == 0) g[nr][nc] = 1;
-                            q.push({{nr,nc},{{cti-1,cd+1},0}});
+                            q.push({{nr,nc},{{cti-1,cd+1},type}});
                         }
                     }
                 }
             }
         }
+        // for(int i=0;i<R;i++){
+        //     for(int j=0;j<C;j++){
+        //         cout << g[i][j] << "\t";
+        //     }
+        //     cout << "\n";
+        // }
+        bool ans = false;
         for(int i=0;i<R;i++){
             for(int j=0;j<C;j++){
-                cout << g[i][j] << "\t";
+                if(g[i][j] == 1) {
+                    ans = true;
+                }
             }
-            cout << "\n";
         }
-
+        if(ans) cout << "YES" << '\n';
+        else cout << "NO" << "\n";
     }
 }
