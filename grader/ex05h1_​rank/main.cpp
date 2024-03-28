812bb45f1de14ed49b7dbab62d995ba29d5fce53
tanasinp <wavet888@gmail.com>
2024-03-28 14:54:08 +0700

    .

---
+++ grader/ex05h1_​rank/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+int code[10101],p[10101],s[10101];
+int n,k;
+int main(){
+    cin >> n >> k;
+    for(int i=0;i<n;i++){
+        cin >> code[i];
+        p[i] = -1;
+        s[i] = 1;
+    }
+}
