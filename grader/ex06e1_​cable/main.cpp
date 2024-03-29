6bff99e13825099efcb86deb64fecc5f8f142c9e
tanasinp <wavet888@gmail.com>
2024-03-29 19:42:25 +0700

    .

--- grader/ex06e1_​cable/main.cpp
+++ grader/ex06e1_​cable/main.cpp
@@ -4,17 +4,46 @@
 #include<queue>
 using namespace std;
 int n;
-int al[1010][1010];
-bool visited[1010];
-int d[1010];
+vector<pair<int,pair<int,int>>> edges;
+int p[1010],s[1010];
+int ans = 0;
+int findset(int x){
+    if(p[x] == -1) return x;
+    p[x] = findset(p[x]);
+    return p[x];
+}
+void unionset(int x,int y){
+    int s1 = findset(x);
+    int s2 = findset(y);
+    if(s[s1] > s[s2]){
+        p[s2] = s1;
+        s[s1] += s[s2];
+    } else {
+        p[s1] = s2;
+        s[s2] += s[s1];
+    }
+}
 int main(){
     cin >> n;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            int x; cin >> x;
-                al[i][j] = x;
-                al[j][i] = x;
+            int x; cin >> x;    
+            edges.push_back({x,{i,j}});
         }
     }
-    
+    sort(edges.begin(),edges.end());
+    for(int i=0;i<n;i++){
+        s[i] = 1;
+        p[i] = -1;
+    }
+    for(int i=0;i<edges.size();i++){
+        int val = edges[i].first;
+        int x = edges[i].second.first;
+        int y = edges[i].second.second;
+        if(findset(x) != findset(y)){
+            ans += val;
+            unionset(x,y);
+        }
+    }
+    cout << ans << "\n";
 }
