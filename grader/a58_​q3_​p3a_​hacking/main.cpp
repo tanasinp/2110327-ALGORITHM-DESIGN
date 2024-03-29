1c427ee43bca8d6dd84d1cf3474b6674621efb6d
tanasinp <wavet888@gmail.com>
2024-03-30 00:43:52 +0700

    dijkstra

--- grader/a58_​q3_​p3a_​hacking/main.cpp
+++ grader/a58_​q3_​p3a_​hacking/main.cpp
@@ -3,44 +3,46 @@
 #include<queue>
 #include<set>
 #include<algorithm>
-#include<queue>
-int n,m,k;
-int dist[1010];
-int prev[1010];
-int st[101010],t[101010];
-vector<int> al[101010],sp;
 using namespace std;
-iny main(){
+int n, m, k;
+int st[100010], t[100010];
+vector<int> al[100010], sp;
+
+
+int main() {
     cin >> n >> m >> k;
     sp.resize(n);
     for(int i=0;i<n;i++) sp[i] = INT_MAX;
+
     for(int i=0;i<k;i++) cin >> st[i];
     for(int i=0;i<n;i++) cin >> t[i];
-    for(int i=0;i<m;i++){
-        int a,b; cin >> a >> b;
+    for(int i=0;i<m;i++) {
+        int a, b;
+        cin >> a >> b;
         al[a].push_back(b);
         al[b].push_back(a);
     }
-    priority_queue<pair<int,int>> pq;
-    for(int i=0;i<k;++){
-        pq.push({-t[i],st[i]});
+    priority_queue<pair<int, int> > pq;
+    for(int i=0;i<k;i++) {
+        pq.push({-t[st[i]], st[i]});
         sp[st[i]] = t[st[i]];
     }
-    while(!q.empty()){
+    while(!pq.empty()) {
         int cost = -pq.top().first;
         int node = pq.top().second;
-        for(int i=0;i<al[node].size();i++){
+        pq.pop();
+        for(int i=0;i<al[node].size();i++) {
             int nn = al[node][i];
             int newcost = cost + t[nn];
-            if(sp[nn] > newcost){
+            if(sp[nn] > newcost) {
                 sp[nn] = newcost;
-                pq.push({-newcost,nn});
+                pq.push({-newcost, nn});
             }
         }
     }
     int ans = 0;
-    for(int i=0;i<n;i++){
-        ans = max(ans.sp[i]);
+    for(int i=0;i<n;i++) {
+        ans = max(ans, sp[i]);
     }
     cout << ans << "\n";
 }
