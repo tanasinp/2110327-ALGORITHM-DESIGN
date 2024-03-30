36d45a40c35ba15d067d612fdab5d4b9ca8d5643
tanasinp <wavet888@gmail.com>
2024-03-31 01:49:41 +0700

    fw

--- grader/a65_​q3a_​expressway/main.cpp
+++ grader/a65_​q3a_​expressway/main.cpp
@@ -4,23 +4,31 @@
 #include<algorithm>
 #include<queue>
 using namespace std;
-int n;
-vector<int> c[701][701];
+int n,x;
+int dist[701][701];
 int main(){
-    cin >> n;
-    cin >> c[1][2];
-    c[2][1] = c[1][2];
+    cin >> n >> x;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++) dist[i][j] = 10000000;
+    }
+    dist[1][2] = x;
+    dist[2][1] = x;
     for(int i=3;i<=n;i++){
         int t; cin >> t;
         while(t--){
             int nn,d; cin >> nn >> d;
-            c[i][nn] = d;
-            c[nn][i] = d;
+            dist[i][nn] = d;
+            dist[nn][i] = d;
         }
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+        }
+        if(k>=3){
+            cout << dist[1][2] << "\n";
         }
     }
 }
