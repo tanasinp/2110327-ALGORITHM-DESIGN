3e21e3e51da659a1a737c5ef529e8d8e8aad69d5
tanasinp <wavet888@gmail.com>
2024-04-18 18:27:21 +0700

    .

---
+++ grader/a66_​q3a_​hex_​map/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int r,c;
+int str,stc,edr,edc;
+int main(){
+    cin >> r >> c;
+    cin >> str >> stc > edr >> edc;
+    int cost[r+1][c+1];
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            cin >> cost[i][j];
+        }
+    }
+    
+}
---
+++ grader/a66_​q3a_​sandworm/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int r,c,k;
+int main(){
+    cin >> r >> c >> k;
+    
+}
