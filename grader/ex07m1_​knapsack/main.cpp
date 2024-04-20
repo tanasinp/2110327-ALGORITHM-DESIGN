f56fd6957e07de9ee4c9aed8ec74171c40bf0595
tanasinp <wavet888@gmail.com>
2024-04-20 13:39:09 +0700

    .

--- grader/ex07m1_​knapsack/main.cpp
+++ grader/ex07m1_​knapsack/main.cpp
@@ -5,31 +5,47 @@
 using namespace std;
 int n;
 double W;
-double v[101],w[101],tail[101];
+double v[101],w[101];
 double ans = 0;
 int sol[101];
+vector<pair<double,pair<double,double>>> r;
+bool customComparator(const pair<double, pair<double, double>>& a, const pair<double, pair<double, double>>& b) {
+    return a.first > b.first; 
+}
+double bestPrice(int idx,double weight){
+    double tmp = 0.0000;
+    for(idx;idx<n;idx++){
+        if(weight < r[idx].second.second){
+            tmp += weight*r[idx].first;
+            break;
+        }else {
+            tmp += r[idx].second.first;
+            weight -= r[idx].second.second;
+        }
+    }
+    return tmp;
+}
 void knapsack(int step,double sumP,double sumW){
     if(sumW > W) return;
-    if(sumP + tail[step] < ans) return;
+    // if(sumP + bestPrice(step,W-sumW) <= ans) return;
+    if(sumP > ans && sumW <= W) ans = sumP;
     if(step < n){
         sol[step] = 1;
-        knapsack(step+1,sumP+v[step],sumW+w[step]);
+        knapsack(step+1,sumP+r[step].second.first,sumW+r[step].second.second);
         sol[step] = 0;
         knapsack(step+1,sumP,sumW);
-    }else {
-        if(sumP > ans && sumW <= W) ans = sumP;
-        // ans = max(ans,sumP);
     }
 }   
 int main(){
     cin >> W >> n;
     for(int i=0;i<n;i++) cin >> v[i];
     for(int i=0;i<n;i++) cin >> w[i];
+    for(int i=0;i<n;i++) r.push_back({v[i]/w[i],{v[i],w[i]}});
 
-    tail[n-1] = v[n-1];
-    for(int i=n-2;i>=0;i--) tail[i] = tail[i+1] + v[i];
+    sort(r.begin(),r.end(),customComparator);
 
     knapsack(0,0,0);
-    cout << ans << "\n";
+    // cout << ans << "\n";
+    printf("%.4lf",ans);
 
 }
